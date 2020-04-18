dcdc5f291adb93e3e97fb89ec80f2a1f9eca9193
Somsai Chittakhone <chittaks02@Student.psdr3.org>
2020-04-17 19:41:32 -0500

    learning

--- first_game.py
+++ first_game.py
@@ -1,1 +1,28 @@
 import pygame
+pygame.init()
+
+black = ( 0, 0, 0)
+white = ( 255, 255, 255)
+green = ( 0, 255, 0)
+red = ( 255, 0, 0)
+
+screen = pygame.display.set_mode(700,500)
+pygame.display.set_caption('First Game')
+
+carryOn = True
+
+clock = pygame.time.Clock()
+
+while carryOn == True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+              carryOn = False 
+
+     screen.fill(WHITE)
+
+     pygame.display.flip()
+     
+     clock.tick(60)
+
+pygame.quit()
