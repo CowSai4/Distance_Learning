07fb28709c497c6f99a856c358fe770fd4ed7cfb
Somsai Chittakhone <chittaks02@Student.psdr3.org>
2020-04-24 23:09:51 -0500

    Changing the car

--- car.py
+++ car.py
@@ -1,16 +1,11 @@
 import pygame
 
 white = ( 255, 255, 255)
+black = ( 0, 0, 0)
 
 class Car(pygame.sprite.Sprite):
-    
-    def __init__(self, color, width, height):
-        super().__init__()
-        
-        self.image = pygame.Surface([width, height])
-        self.image.fill(WHITE)
-        self.image.set_colorkey(WHITE)
- 
-        pygame.draw.rect(self.image, color, [0, 0, width, height])
-        
-        self.rect = self.image.get_rect()
+    def __init__(self):
+        super(Car,self).__init__()
+        self.surf = pygame.Surface((75,25))
+        self.surf.fill(white)
+        self.rect = self.surf.get_rect()   
